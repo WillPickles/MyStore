c5ffbd185bf8fb0c1010a1462fea14a727b47854
Will Pickles <will@willpickles.co.uk>
2023-04-04 20:48:41 +0100

    Finished Section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,6 +2,7 @@ import Catalog from "../../features/catalog/Catalog";
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Header from "./Header";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -23,7 +24,8 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
       <Container>
-        <Catalog />
+        <Outlet />
+
       </Container>
     </ThemeProvider>
   );
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,22 +1,92 @@
-import { AppBar, FormControlLabel, FormGroup, Switch, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  FormGroup,
+  IconButton,
+  List,
+  ListItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import DarkModeSwitch from "../Components/DarkModeSwitch";
+import { NavLink } from "react-router-dom";
+import { ShoppingCart } from "@mui/icons-material";
+
+const midLinks = [
+  { title: "catalog", path: "/catalog" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" },
+];
+
+const rightLinks = [
+  { title: "login", path: "/login" },
+  { title: "register", path: "/register" },
+];
 
 interface Props {
-    darkMode: boolean;
-    toggleDarkMode: () => void;
-
+  darkMode: boolean;
+  toggleDarkMode: () => void;
 }
 
-export default function Header({darkMode, toggleDarkMode}: Props) {
-    return (
-        <AppBar position="static" sx={{ mb: 4 }}>
-            <Toolbar>
-                <Typography variant="h6">
-                    MY-STORE
-                </Typography>
-                <FormGroup sx={{ ml: 4 }}>
-                    <FormControlLabel control={<Switch checked={darkMode} onChange={toggleDarkMode} />} label="Dark Mode" />
-                </FormGroup>
-            </Toolbar>
-        </AppBar>
-    )
+const navStyles = {
+  color: "inherit",
+  typography: "h6",
+  "&:hover": {
+    color: "grey.500",
+  },
+  "&.active": {
+    color: "text.secondary",
+  },
+};
+
+export default function Header({ darkMode, toggleDarkMode }: Props) {
+  return (
+    <AppBar position="static" sx={{ mb: 4 }}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box display='flex' alignItems='center'>
+          <Typography variant="h6" component={NavLink} to="/">
+            MY-STORE
+          </Typography>
+
+          <FormGroup sx={{ ml: 4 }}>
+            <DarkModeSwitch
+              darkMode={darkMode}
+              toggleDarkMode={toggleDarkMode}
+            />
+          </FormGroup>
+        </Box>
+
+        <List sx={{ display: "flex" }}>
+          {midLinks.map(({ title, path }) => (
+            <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+
+        <Box display='flex' alignItems='center'>
+          <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+            <Badge badgeContent="4" color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
